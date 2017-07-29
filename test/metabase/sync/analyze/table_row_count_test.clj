00da0d55b092702b7ceb300c4765606b36684f49
Cam Saul <cammsaul@gmail.com>
2017-07-28 21:00:23 -0700

    Sync refactor :sunglasses: [ci drivers]

---
+++ test/metabase/sync/analyze/table_row_count_test.clj
@@ -0,0 +1,21 @@
+(ns metabase.sync.analyze.table-row-count-test
+  "Tests for the sync logic that updates a Table's row count."
+  (:require [metabase
+             [query-processor-test :as qp-test]
+             [util :as u]]
+            [metabase.models.table :refer [Table]]
+            [metabase.sync.analyze.table-row-count :as table-row-count]
+            [metabase.test.data :as data]
+            [toucan.db :as db]
+            [toucan.util.test :as tt]
+            [metabase.test.data.datasets :as datasets]))
+
+;; test that syncing table row counts works
+;; TODO - write a Druid version of this test. Works slightly differently since Druid doesn't have a 'venues' table
+;; TODO - not sure why this doesn't work on Oracle. Seems to be an issue with the test rather than with the Oracle driver
+(datasets/expect-with-engines (disj qp-test/non-timeseries-engines :oracle)
+  100
+  (tt/with-temp Table [venues-copy (let [venues-table (Table (data/id :venues))]
+                                     {:name (:name venues-table), :db_id (:db_id venues-table), :rows 0})]
+    (table-row-count/update-row-count-for-table! venues-copy)
+    (db/select-one-field :rows Table :id (u/get-id venues-copy))))
